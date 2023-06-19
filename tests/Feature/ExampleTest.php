198b556cb3920acdc8eee7a47e591f999b0a926b
Emanuele Coppola <emanuele.coppola99@gmail.com>
2023-06-19 06:02:24 +0200

    Add api test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,46 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Translation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
+    const TEST_TRANSLATION = [
+        'key' => 'test-key',
+        'labels' => [
+            'it' => 'Stringa di test',
+            'en' => 'Test string',
+            'fr' => 'Chaîne de texte'
+        ]
+    ];
+
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_translation_store_api(): void
     {
-        $response = $this->get('/');
+        $response = $this->post(route('translation.store'), self::TEST_TRANSLATION);
 
-        $response->assertStatus(200);
+        $response->assertStatus(201);
+        $response->assertContent(
+            json_encode(self::TEST_TRANSLATION)
+        );
+
+        $newTranslation = Translation::with(['labels'])->where('key', '=', self::TEST_TRANSLATION['key'])->first();
+        $newLabels = $newTranslation->labels->keyBy('lang');
+
+        $this->assertModelExists($newTranslation);
+
+        foreach (array_keys(self::TEST_TRANSLATION['labels']) as $lang) {
+            $newLabel = $newLabels->get($lang);
+
+            $this->assertModelExists($newLabel);
+            $this->assertSame($newLabel->lang, $lang);
+            $this->assertSame($newLabel->text, self::TEST_TRANSLATION['labels'][$lang]);
+        }
     }
 }
