34229a52790a14ffec28dbdc29b1ed335033a889
Emanuele Coppola <emanuele.coppola99@gmail.com>
2023-06-19 04:30:19 +0200

    Add custom api exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +29,32 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (ValidationException $exception, Request $request) {
+            if ($request->is('api/*')) {
+                $validator = $exception->validator;
+
+                return response()->json(
+                    data: ['errors' => $validator->errors()],
+                    status: ResponseCode::HTTP_UNPROCESSABLE_ENTITY
+                );
+            }
+        });
+
+        $this->renderable(function (HttpException $exception, Request $request) {
+            $message = $exception->getMessage();
+
+            if (!$message) {
+                $message = ResponseCode::$statusTexts[$exception->getStatusCode()];
+            }
+
+            if ($request->is('api/*')) {
+                return response()->json(
+                    data: ['error' => $message],
+                    status: $exception->getStatusCode()
+                );
+            }
         });
     }
 }
